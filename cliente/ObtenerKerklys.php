ff9f2d692ab8b8d3da5b448f1e670fa19c6c8140
Manuel Rosas <josem_rl@hotmail.com>
2022-04-21 22:09:46 -0500

    Commit get kerkly

--- cliente/ObtenerKerklys.php
+++ cliente/ObtenerKerklys.php
@@ -4,8 +4,16 @@
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $nombreO = $_GET['nombreO'];
 
-        $consulta = "SELECT kerkly.Curp, kerkly.Nombre, kerkly.Apellido_Paterno, kerkly.Apellido_Materno from oficios INNER JOIN 
-        oficio_kerkly on oficios.idOficio = oficio_kerkly.id_oficioK INNER JOIN kerkly on kerkly.Curp = oficio_kerkly.id_kerklyK WHERE 
+        $consulta = "SELECT kerkly.Curp,
+        kerkly.Nombre,
+        kerkly.Apellido_Paterno,
+        kerkly.Apellido_Materno,
+        direccion.latitud,
+        direccion.longitud
+        from oficios INNER JOIN oficio_kerkly on oficios.idOficio = oficio_kerkly.id_oficioK
+        INNER JOIN kerkly on kerkly.Curp = oficio_kerkly.id_kerklyK INNER JOIN
+        direccionkerkly on kerkly.Curp = direccionkerkly.idKerkly INNER JOIN
+        direccion ON direccion.idDireccion = direccionkerkly.idDireccion WHERE
         oficios.nombreO = '$nombreO';";
         
         $check = mysqli_query($Conexion,$consulta);

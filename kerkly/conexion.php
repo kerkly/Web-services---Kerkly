836889801c53a922762240d63f888866cdcf9fb5
KevinHdezVaz <kevinhdezvaz@gmail.com>
2022-09-18 11:05:29 -0500

    updateCredenciales

--- kerkly/conexion.php
+++ kerkly/conexion.php
@@ -1,17 +1,13 @@
 <?php
 
-$servidor= "192.168.130.37";
-$NombreDB = "kerkly";
-$UsaurioDB = "bdsaua";
-$ContrasenaDB = "2Ut&E=adrAY7";
-//$NombreDB = "kerkly";
-//$UsaurioDB = "AdminKerkly";
-//$ContrasenaDB = "admin kerkly001";
+$servidor= "localhost";
+$NombreDB = "bdsaua";
+$UsaurioDB = "usrsaua";
+$ContrasenaDB = "AmorMio15$&";
 
 $Conexion = mysqli_connect($servidor, $UsaurioDB, $ContrasenaDB,$NombreDB);
+
 if($Conexion->connect_error){
     die("fallo la conexion" .$Conexion->connect_error);
-
 }
-
 ?>
